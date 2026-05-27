617ed94a43d3fb5700a1b0c5fe838d8603c08dc4
pranay kollu <kollup1@wvhs.wlwv.k12.or.us>
2026-05-26 20:26:49 -0700

    updated for advantage kit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "command_base_robot_v3";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "9207da577559b5574f0829acd2f8941639917408";
+  public static final String GIT_DATE = "2026-05-26 22:54:55 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-05-26 23:25:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1779852326578L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
